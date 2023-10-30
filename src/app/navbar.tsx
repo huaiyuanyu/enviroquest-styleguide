24479e47ea7858e603635c28fef4708f1394e4d4
Gary Yu <hyu100@my.bcit.ca>
2023-10-30 15:22:11 -0700

    set up basic navbar

---
+++ src/app/navbar.tsx
@@ -0,0 +1,18 @@
+export default function Navbar () {
+    return (
+        <div className="flex flex-row justify-items-center place-items center h-auto w-full fixed top-0 bg-[#7C4B13] text-[#69CA29] font-bold">
+            <div className="p-3">
+                Link 1
+            </div>
+            <div className="p-3">
+                Link 2
+            </div>
+            <div className="p-3">
+                Link 3
+            </div>
+            <div className="p-3">
+                Link 4
+            </div>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 import Image from 'next/image'
 
 export default function Home() {
+
+  const lorem = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto earum incidunt, nulla ea nostrum maxime voluptatem neque tempore impedit minima libero accusantium odit sed dicta a beatae voluptatibus dolores pariatur.";
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Enviroquest Styleguide</h1>
+      <p>{lorem}</p>
+      <p>{lorem}</p>
+      <p>{lorem}</p>
+      <p>{lorem}</p>
+      <p>{lorem}</p>
+      <p>{lorem}</p>
+      <p>{lorem}</p>
+      <p>{lorem}</p>
+      <p>{lorem}</p>
+      <p>{lorem}</p>
     </main>
   )
 }
