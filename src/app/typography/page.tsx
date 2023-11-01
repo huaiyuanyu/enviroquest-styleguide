33e48f46c7a93d96f4344664f141c158214dd243
Gary Yu <hyu100@my.bcit.ca>
2023-10-31 23:37:08 -0700

    organize headers into two different rows and apply font to them

--- src/app/typography/page.tsx
+++ src/app/typography/page.tsx
@@ -2,45 +2,24 @@ export default function Typography () {
     return (
         <div className="mt-[50px]">
             <p>Typography Info</p>
-            <div className="flex flex-col w-full">
-                <div className="w-5/6">
-                    <h1>EnviroQuest Header</h1>
-                </div>
-                <div>
-                    <h1>Header 1 Size</h1>
-                </div>
-                <div className="w-5/6">
-                    <h2>EnviroQuest Header</h2>
-                </div>
-                <div className="w-1/6">
-                    <h2>Header 2 Size</h2>
-                </div>
-                <div className="w-5/6">
-                    <h3>EnviroQuest Header</h3>
-                </div>
-                <div className="w-1/6">
-                    <h3>Header 3 Size</h3>
-                </div>
-                <div className="w-5/6">
-                    <h4>EnviroQuest Header</h4>
-                </div>
-                <div className="w-1/6">
-                    <h4>Header 4 Size</h4>
-                </div>
-                <div className="w-5/6">
-                    <h5>EnviroQuest Header</h5>
-                </div>
-                <div className="w-1/6">
-                    <h5>Header 5 Size</h5>
-                </div>
-                <div className="w-5/6">
-                    <h6>EnviroQuest Header</h6>
-                </div>
-                <div className="w-1/6">
-                    <h6>Header 6 Size</h6>
-                </div>
+            <div className="flex w-full font-mono">
+            <div className="w-4/6 bg-gray-400 h-auto">
+                <div><h1>Header 1</h1></div>
+                <div><h2>Header 2</h2></div>
+                <div><h3>Header 3</h3></div>
+                <div><h4>Header 4</h4></div>
+                <div><h5>Header 5</h5></div>
+                <div><h6>Header 6</h6></div>
             </div>
-            
+            <div className="w-2/6 bg-gray-500 h-auto">
+                <div><h1>Header 1 Size</h1></div>
+                <div><h2>Header 2 Size</h2></div>
+                <div><h3>Header 3 Size</h3></div>
+                <div><h4>Header 4 Size</h4></div>
+                <div><h5>Header 5 Size</h5></div>
+                <div><h6>Header 6 Size</h6></div>
+            </div>
+            </div>
         </div>
     )
 }
