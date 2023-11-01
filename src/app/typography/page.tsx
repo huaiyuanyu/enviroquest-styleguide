abd63c36f23f86f3b3883e7942ed8abaf9040fb7
Gary Yu <hyu100@my.bcit.ca>
2023-10-31 23:26:53 -0700

    set up basic header sizes content

--- src/app/typography/page.tsx
+++ src/app/typography/page.tsx
@@ -2,6 +2,45 @@ export default function Typography () {
     return (
         <div className="mt-[50px]">
             <p>Typography Info</p>
+            <div className="flex flex-col w-full">
+                <div className="w-5/6">
+                    <h1>EnviroQuest Header</h1>
+                </div>
+                <div>
+                    <h1>Header 1 Size</h1>
+                </div>
+                <div className="w-5/6">
+                    <h2>EnviroQuest Header</h2>
+                </div>
+                <div className="w-1/6">
+                    <h2>Header 2 Size</h2>
+                </div>
+                <div className="w-5/6">
+                    <h3>EnviroQuest Header</h3>
+                </div>
+                <div className="w-1/6">
+                    <h3>Header 3 Size</h3>
+                </div>
+                <div className="w-5/6">
+                    <h4>EnviroQuest Header</h4>
+                </div>
+                <div className="w-1/6">
+                    <h4>Header 4 Size</h4>
+                </div>
+                <div className="w-5/6">
+                    <h5>EnviroQuest Header</h5>
+                </div>
+                <div className="w-1/6">
+                    <h5>Header 5 Size</h5>
+                </div>
+                <div className="w-5/6">
+                    <h6>EnviroQuest Header</h6>
+                </div>
+                <div className="w-1/6">
+                    <h6>Header 6 Size</h6>
+                </div>
+            </div>
+            
         </div>
     )
 }
