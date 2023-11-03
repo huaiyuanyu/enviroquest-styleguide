3f453644207587ef8ba1df819f7e91db2620c77a
Gary Yu <hyu100@my.bcit.ca>
2023-11-03 15:49:31 -0700

    fill up content for typography page

--- src/app/typography/page.tsx
+++ src/app/typography/page.tsx
@@ -6,13 +6,14 @@ export default function Typography () {
 
     const lorem = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
 
-    const headerFontDescription = ""
-    const bodyFontDescription = ""
+    const headerFontDescription = "Abril Fatface is our standard serif font for headers and subtitles in EnviroQuest. It gives off a strong impression with the thickness of the lines, while still be relatively neutral in terms of form. Impact is projected this way to the readers, grabbing their attention and emphasizing each individual header. With this, we're able to impress the importance of the content upon the reader. Further more, Abril Fatface is inspired by the titling fonts found in old advertising posters, making it very useful for readers who are scrolling through our Information page, looking for particular content."
+
+    const bodyFontDescription = "For our sans serif font, Open Sans was used, as it's a flexible, light font that is optimized for print, web, and mobile interfaces, making it easily legible in any situation. With extra spacing between each character, the font makes the process of reading on a screen smoother, which in turn helps the reader go through information-dense pages with minimal stress. As well, the contrast between our header font versus our body font goes a fair distance in helping users easily distinguish between what's necessary and what's not."
 
     const [font, setFont] = useState("flex w-full font-header");
     const [button1, setButtonOne] = useState("font-header px-5 bg-gray-400");
     const [button2, setButtonTwo] = useState("font-body px-5");
-    const [text, setText] = useState(lorem);
+    const [para, setText] = useState(headerFontDescription);
 
     function handleClick (e:any) {
         const value = e.target.value;
@@ -22,9 +23,11 @@ export default function Typography () {
         if(name == "header") {
             setButtonOne("font-header px-5 bg-gray-400");
             setButtonTwo("font-body px-5");
+            setText(headerFontDescription)
         } else {
             setButtonOne("font-header px-5");
             setButtonTwo("font-body px-5 bg-gray-400");
+            setText(bodyFontDescription)
         }
 
         setFont(text);
@@ -43,7 +46,7 @@ export default function Typography () {
             </div>
             <div className="p-4">
                 <p className="text-l">
-                    {text}
+                    {para}
                 </p>
             </div>
             <div className={font}>
